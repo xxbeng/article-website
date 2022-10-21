0739d02055bc10c584ef0101fa2434b5f4ae36dc
Andrew Meads <48777624+andrew-meads@users.noreply.github.com>
2022-10-21 15:48:10 +1300

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+/**
+ * Main application file.
+ * 
+ * NOTE: This file contains many required packages, but not all of them - you may need to add more!
+ */
+
+// Setup Express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Setup Handlebars
+const handlebars = require("express-handlebars");
+app.engine("handlebars", handlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Setup body-parser
+app.use(express.urlencoded({ extended: false }));
+
+// Setup cookie-parser
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// Make the "public" folder available statically
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+// Use the toaster middleware
+app.use(require("./middleware/toaster-middleware.js"));
+
+// Setup routes
+app.use(require("./routes/application-routes.js"));
+
+// Start the server running.
+app.listen(port, function () {
+    console.log(`App listening on port ${port}!`);
+});
