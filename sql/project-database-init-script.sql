21e528004aaafe93f1082192836c5e29e3daab7d
Hongwei <tanhongwei33130@gmail.com>
2022-11-03 20:48:37 +1300

    sql update

--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -5,6 +5,9 @@
  */
 
 -- drop table if exists test;
+drop table if exists cToC;
+drop table if exists comments;
+drop table if exists articles;
 drop table if exists users;
 
 /* create table test (
@@ -29,3 +32,29 @@ create table users (
 	description varchar(500),
     authToken varchar(128)
 );
+
+create table articles (
+	id INTEGER not null primary key AUTOINCREMENT,
+	title varchar(64) not null,
+	content text not null,
+	timestamp timestamp default CURRENT_TIMESTAMP,
+	userId INTEGER not null,
+	foreign key (userId) REFERENCES users (id)
+);
+
+create table comments (
+	id integer NOT NULL,
+	content varchar(200) not null,
+	datenTime timestamp,
+	articleId integer NOT NULL, 
+	primary KEY(id),
+	foreign key(articleId) REFERENCES articles (id)
+);
+
+create TABLE cToC (
+	cReceiverId integer NOT NULL,
+	cSenderId integer NOT NULL,
+	FOREIGN KEY (cReceiverId) REFERENCES comments (id),
+	FOREIGN KEY (cSenderId) REFERENCES comments (id)
+);
+
